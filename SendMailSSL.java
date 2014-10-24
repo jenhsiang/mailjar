0e208e49b8e5613ce2393704ae2335ef8792d10f
JenHsiangChen <chentai008@yahoo.com>
2014-10-24 12:39:02 +0800

    Create SendMailSSL.java

---
+++ SendMailSSL.java
@@ -0,0 +1,50 @@
+package testemail2;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMailSSL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+ 
+		Session session = Session.getDefaultInstance(props,
+			new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication("test@test.com","12345678");
+				}
+			});
+ 
+		try {
+ 
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("chentai008@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse("chentai008@yahoo.com"));
+			message.setSubject("Testing Subject123«¢«¢");
+			message.setText("Dear Mail Crawler," +
+					"\n\n No spam to my email, please!");
+ 
+			Transport.send(message);
+ 
+			System.out.println("Done");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
